69bfd199fc43db195a8031afef453381f0e7d5c4
Ionfinisher <teddyassih@gmail.com>
2023-12-16 20:35:41 +0000

    deleted some unused code

--- it_academia/academia/urls.py
+++ it_academia/academia/urls.py
@@ -6,8 +6,6 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    # path("", views.home, name="home"),
-    # path("hello/<name>", views.hello_there, name="hello_there"),
     path("", views.home, name="home"),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
